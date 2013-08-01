b2be7bace0852a870a0dc4fa63be449b6b307e4f
Tyrone <ty@crayoncapsule.co.za>
2013-08-01 09:51:16 +0200

    first commit

---
+++ js/main-ck.js
@@ -0,0 +1,5 @@
+//===============================================================
+//                    CYCLE SLIDER
+//===============================================================
+//==== SLIDESHOW IMAGES ON HOME PAGE
+$(".slideshow").cycle({fx:"scrollLeft",timeout:0,next:"#next",prev:"#prev"});
